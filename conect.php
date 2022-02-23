ced3320c6412468a709cd5e9b18c28625adf79c1
dilshan79 <buddhidilshan79@gmail.com>
2022-02-23 18:32:31 +0530

    create php file and design login page

---
+++ conect.php
@@ -0,0 +1,58 @@
+<?php
+    $FirstName = $_POST['FirstName'];
+    $LastName = $_POST['LastName'];
+    $Dob = $_POST['Dob'];
+    $Address = $_POST['Address'];
+    $ContacNumber = $_POST['ContacNumber'];
+    $Gender = $_POST['Gender'];
+    $Email = $_POST['Email'];
+    $Password = $_POST['Password'];
+    $Subject = $_POST['Subject'];
+
+    //database connection
+    $conn =new mysqli('localhost','root','','userdb');
+    if($conn->connect_error){
+        die('connection Failed : '.$conn->connect_error);
+
+    }
+    else{
+        $stmt = $conn->prepare("insert into registration(FirstName,LastName,Dob,Address,ContacNumber,Gender,Email,Password,Subject)
+        values(?,?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("s,s,s,s,i,s,s,s,s",$FirstName,$LastName,$Dob,$Address,$ContacNumber,$Gender,$Email,$Password,$Subject);
+        $stmt->execute();
+        echo "registration successfully...";
+        $stmt->close();
+        $conn->close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ database.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>user registration  php</title>
+</head>
+<body>
+    
+</body>
+</html>
