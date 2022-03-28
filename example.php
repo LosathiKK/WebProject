24c0d84e027ce7628dd831db70c015af24e0f307
dilshan79 <buddhidilshan79@gmail.com>
2022-03-28 14:49:31 +0530

    create and modified

---
+++ example.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html>
+    <style>
+    *{
+        box-sizing: border-box;
+    }
+    input[type=text],input[type=password],input[type=email], select {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+      font-size: large;
+      border-color: blue;
+}
+input[type=submit] {
+      width: 100%;
+     background-color: #4CAF50;
+     color: white;
+     padding: 14px 20px;
+     margin: 8px 0;
+     border: none;
+     border-radius: 4px;
+     cursor: pointer;
+}
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  
+}
+
+
+    </style>
+    
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+
+    </head>
+    <body style="background-image: url('beautiful-woman-studying-online-home_109710-3281.jpg');
+               background-repeat: no-repeat;
+               background-size:cover;
+               background-attachment: fixed;
+               background-position: top right;
+               opacity: 0.8;">
+
+        <div class="transbox" style=" width:50%; margin-left:auto; margin-right: auto; 
+               background-color: #ffffff;
+               border: 1px solid black;
+               opacity: 0.8;
+               ">
+            <h1 style="text-align: center;">LOGIN FORM</h1>
+            <form action="example2.html" target="_blank" method="get" novalidate style="width:50%; margin-left: auto; margin-right: auto;">
+                <label for="email">Email:</label><br>
+                <input type="email" id="emai" name="email"><br>
+                <label for="pwd">Last name:</label><br>
+                <input type="password" id="pwd" name="pwd">
+                <input type="submit" value="Submit" name="submit1"
+                ><br>
+                <a href="example2.html">sinup</a>
+
+
+                
+            </form>
+
+
+
+
+        </div>
+       
+
+    </body>
+    
+</html>
